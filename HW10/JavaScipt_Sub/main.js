2290e0b91e2bd019cc8dd303922d1ba6b5df469c
WesamAlqudah <100211002+WesamAlqudah@users.noreply.github.com>
2022-03-10 08:57:43 -0600

    .

--- HW10/JavaScipt_Sub/main.js
+++ HW10/JavaScipt_Sub/main.js
@@ -4,9 +4,9 @@ import {person} from "./Module/person.js";
 import { Employee } from "./Module/Employee.js";
 
 const Person=[
-    new person("Anna Smith",new Date(1998,12,15)),
-    new person("Bob Jone",new Date(1945,11,16)),
-    new person("Carlos Slim Helu",new Date(1976,9,24))
+    new person("Anna Smith",new Date(1998,11,15)),
+    new person("Bob Jone",new Date(1945,10,16)),
+    new person("Carlos Slim Helu",new Date(1976,8,24))
 ];
 for(const Per of Person) {
     console.log(Per.toString());
