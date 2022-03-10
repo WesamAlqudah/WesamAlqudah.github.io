4672aa83d70fc38454b4c46fdc053c5b9e4e3663
WesamAlqudah <100211002+WesamAlqudah@users.noreply.github.com>
2022-03-10 01:28:52 -0600

    .

--- HW10/JavaScipt_Sub/Module/person.js
+++ HW10/JavaScipt_Sub/Module/person.js
@@ -9,14 +9,16 @@ export class person {
     getName(){return this.name;}
     setName(name){this.name=name;}
     getDateOfBirth(){return this.dateOfBirth;}
-    setDateOfBirth(dateOfBirth){ this.dateOfBirth=dateOfBirth;}
+    //setDateOfBirth(dateOfBirth){ this.dateOfBirth=dateOfBirth;}
     
-//     setDateOfBirth(dateOfBirth){var strArray=['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-//     var d = dateOfBirth.getDate();
-//     var m = strArray[dateOfBirth.getMonth()];
-//     var y = dateOfBirth.getFullYear();
-//     this.dateOfBirth=dateOfBirth= '' + (d <= 9 ? '0' + d : d) + '-' + m + '-' + y;
-//    }
-    //toString = ()=>`Name: ${this.name}, Date Of Birth: ${this.dateOfBirth.toLocaleDateString()}`
-    toString = ()=>`Name: ${this.name}, Date Of Birth: ${this.dateOfBirth.toISOString().slice(0,10)}`
+    setDateOfBirth(dateOfBirth){var strArray=['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    var d = dateOfBirth.getDate();
+    var m = strArray[dateOfBirth.getMonth()];
+    var y = dateOfBirth.getFullYear();
+    //this.dateOfBirth=dateOfBirth= '' + (d <= 9 ? '0' + d : d) + '-' + m + '-' + y;
+    this.dateOfBirth=dateOfBirth= y + '-' + m +'-' + (d <= 9 ? '0' + d : d);
+   }
+    toString = ()=>`Name: ${this.name}, Date Of Birth: ${this.dateOfBirth.toLocaleDateString()}`
+  //  toString = ()=>`Name: ${this.name}, Date Of Birth: ${this.dateOfBirth.toISOString().slice(0,10)}`
+    //toString = ()=>`Name: ${this.name}, Date Of Birth: ${this.dateOfBirth}`
 }
