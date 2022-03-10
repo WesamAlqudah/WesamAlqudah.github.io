59c71d2cef68e3d3688e3d4c7e4b86a1cec54eee
WesamAlqudah <100211002+WesamAlqudah@users.noreply.github.com>
2022-03-10 01:23:22 -0600

    .

--- HW10/JavaScipt_Sub/Module/person.js
+++ HW10/JavaScipt_Sub/Module/person.js
@@ -1,7 +1,7 @@
 //class
-"use strict"
+"use strict";
 
-class person {
+export class person {
     constructor(name,dateOfBirth){
         this.name=name;
         this.dateOfBirth=dateOfBirth;
@@ -9,6 +9,14 @@ class person {
     getName(){return this.name;}
     setName(name){this.name=name;}
     getDateOfBirth(){return this.dateOfBirth;}
-    setDateOfBirth(dateOfBirth){this.dateOfBirth=dateOfBirth;}
-    toString = ()=>`Name: ${this.name}, DateOfBirth: ${this.dateOfBirth.getFullYear()}-${this._dateOfBirth.getMonth() + 1}-${this._dateOfBirth.getDay() + 1}`
+    setDateOfBirth(dateOfBirth){ this.dateOfBirth=dateOfBirth;}
+    
+//     setDateOfBirth(dateOfBirth){var strArray=['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+//     var d = dateOfBirth.getDate();
+//     var m = strArray[dateOfBirth.getMonth()];
+//     var y = dateOfBirth.getFullYear();
+//     this.dateOfBirth=dateOfBirth= '' + (d <= 9 ? '0' + d : d) + '-' + m + '-' + y;
+//    }
+    //toString = ()=>`Name: ${this.name}, Date Of Birth: ${this.dateOfBirth.toLocaleDateString()}`
+    toString = ()=>`Name: ${this.name}, Date Of Birth: ${this.dateOfBirth.toISOString().slice(0,10)}`
 }
--- HW10/JavaScipt_Sub/main.js
+++ HW10/JavaScipt_Sub/main.js
@@ -1,14 +1,17 @@
-"use strict"
+"use strict";
 
 import {person} from "./Module/person.js";
+import { Employee } from "./Module/Employee.js";
 
 const Person=[
-    new person("Anna Smith",new Date(1998-12-15)),
-    new person("Bob Jone",new Date(1945-11-16)),
-    new person("Carlos Slim Helu",new Date(1976-09-24)),
+    new person("Anna Smith",new Date(1998,12,15)),
+    new person("Bob Jone",new Date(1945,11,16)),
+    new person("Carlos Slim Helu",new Date(1976,9,24))
 ];
-Person.map((test)=>console.log(text.toString()));
-
+for(const Per of Person) {
+    console.log(Per.toString());
+}
 //Q2:
 
-//const Emp=new Employee();
+const Emp=new Employee("Jim Hanson",null,245995.00 );
+Emp.doJob("Software Engineer");
