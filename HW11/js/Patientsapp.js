5e43f61ea8b1323c8c0ab30a8f9e1b4e42ad3617
WesamAlqudah <100211002+WesamAlqudah@users.noreply.github.com>
2022-03-11 00:13:30 -0600

    .

---
+++ HW11/js/Patientsapp.js
@@ -0,0 +1,15 @@
+"use strict";
+import { Patients } from "./model/MedicalCenter";
+
+$(document).ready(function(){
+    const patientDara=[];
+
+    $(".form-group").submit(function(event){
+        event.preventDefault();
+        const Patient_ID=$("#PatientID").val();
+        const First_Name= $("#patientIdNumber").val();
+        
+    })
+}
+
+)
