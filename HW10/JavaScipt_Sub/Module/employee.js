7ef6f5567e25368acb325b4a3640667bb2a29dee
WesamAlqudah <100211002+WesamAlqudah@users.noreply.github.com>
2022-03-12 14:45:21 -0600

    .

--- HW10/JavaScipt_Sub/Module/employee.js
+++ HW10/JavaScipt_Sub/Module/employee.js
@@ -2,17 +2,26 @@
 
 import { person } from "./person.js";
 
-export class Employee extends person{
-    #Salary=0.0;
-    #hireDate=null;
-constructor(name, dateOfBirth, Salary, hireDate){
-    super(name,dateOfBirth);
-    this.#Salary=Salary;
-    this.#hireDate=hireDate;
-}
-getSalary(){return this.#Salary;}
-getHireDate(){return this.#hireDate;}
-doJob(jobTitle){
-    console.log(`${this.name} is a ${jobTitle} who earns ${new Intl.NumberFormat("en-US", {style: "currency", currency: "USD"}).format(this.#Salary)}`);
-}
+export class Employee extends person {
+  #Salary = 0.0;
+  #hireDate = null;
+  constructor(name, dateOfBirth, Salary, hireDate) {
+    super(name, dateOfBirth);
+    this.#Salary = Salary;
+    this.#hireDate = hireDate;
+  }
+  getSalary() {
+    return this.#Salary;
+  }
+  getHireDate() {
+    return this.#hireDate;
+  }
+  doJob(jobTitle) {
+    console.log(
+      `${this.name} is a ${jobTitle} who earns ${new Intl.NumberFormat(
+        "en-US",
+        { style: "currency", currency: "USD" }
+      ).format(this.#Salary)}`
+    );
+  }
 }
